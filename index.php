b41e6fed3f5c4dc7179db43c75b38cbaaa0c0eed
chuandongz <411247654@qq.com>
2019-03-29 17:35:24 +0800

    自动构建

--- index.php
+++ index.php
@@ -4,10 +4,16 @@ require "vendor/autoload.php";
 ini_set("display_errors", "On");
 
 error_reporting(E_ALL | E_STRICT);
-$a = md5(md5('123456'));
+/*$a = md5(md5('123456'));
 dd($a);
 dd(md5($a.'gGz9TUd3'));
 $obj = new \App\Pay\Demo(new \App\Pay\Wetch());
 
 $obj->transfer();
-echo 'sdfasdfsadf';
+echo 'sdfasdfsadf';*/
+dd(1);
+$a['dd'] = 0;
+if(isset($a['dd'])){
+    dd(1);
+}
+dd('jia');
